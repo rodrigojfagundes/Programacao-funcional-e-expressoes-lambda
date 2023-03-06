a3bbe26c64ec47f6432e16d9ff2b2b769a22b262
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2023-03-06 19:30:38 -0300

    Programação funcional e calculo lambda
    Aula 253.

---
+++ 253 - Programacao funcional e calculo lambda/1/course/src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.Arrays;
+
+public class Program {
+	
+
+	public static int globalValue = 3;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[]vect = new int[] {3, 4, 5};
+		
+		changeOddValues(vect);
+		
+		System.out.println(Arrays.toString(vect));
+		
+	}
+	
+	public static void changeOddValues(int[] numbers) {
+		for (int i=0; i<numbers.length; i++) {
+			if (numbers[i] % 2 !=0) {
+				numbers[i] +=globalValue;
+			}
+		}
+	}
+}
